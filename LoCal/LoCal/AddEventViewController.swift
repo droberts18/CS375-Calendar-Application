8c38c335b11cdc372d2fb099a8e8412540d529bd
droberts18 <droberts@Drews-MacBook-Pro.local>
2016-03-07 12:42:49 -0800

    Added to the Add Events page
    Still needs finishing

--- LoCal/LoCal/AddEventViewController.swift
+++ LoCal/LoCal/AddEventViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 
 
-class AddEventViewController: UIViewController{
+class AddEventViewController: UIViewController, UITextViewDelegate{
     var backgroundColor = UIColor.whiteColor()
 
     convenience init(backgroundColor: UIColor){
@@ -24,8 +24,11 @@ class AddEventViewController: UIViewController{
         let statusBar = UIView()
         let addEventContainer = UIView()
         //let header = ViewControllerHeader(title: "Add Event", imageFileName: "AddEventButtonPlus.png")
-        let titleTextbox = UITextField()
-        let locationTextbox = UITextField()
+        let titleLabel = UILabel()
+        let nameOfEventLabel = UILabel()
+        let userNameOfEvent = UITextView()
+        let locationTextbox = UILabel()
+        let userLocation = UITextField()
         let startDate = UIDatePicker()
         let endDate = UIDatePicker()
         let eventType = UIImageView()
@@ -36,6 +39,9 @@ class AddEventViewController: UIViewController{
         
         self.view.addSubview(statusBar)
         self.view.addSubview(addEventContainer)
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(nameOfEventLabel)
+        self.view.addSubview(userNameOfEvent)
         
         addEventContainer.addSubview(exitButton)
         let exitButtonImage = UIImage(named: "XButton.png")
@@ -60,6 +66,33 @@ class AddEventViewController: UIViewController{
         addEventContainer.autoPinEdge(.Right, toEdge: .Right, ofView: self.view)
         addEventContainer.autoPinEdge(.Bottom, toEdge: .Bottom, ofView: self.view)
         
+        titleLabel.text = "Add Your Event!"
+        titleLabel.font = UIFont.systemFontOfSize(40)
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.textAlignment = .Center
+        titleLabel.autoPinEdgeToSuperviewEdge(.Right)
+        titleLabel.autoPinEdgeToSuperviewEdge(.Left)
+        titleLabel.autoPinEdge(.Top, toEdge: .Top, ofView: self.view, withOffset: 100)
+        
+//        nameOfEventLabel.text = "Name your event"
+//        nameOfEventLabel.font = UIFont.systemFontOfSize(20)
+//        nameOfEventLabel.textColor = UIColor.whiteColor()
+//        nameOfEventLabel.autoPinEdge(.Right, toEdge: .Right, ofView: self.view, withOffset: 30)
+//        nameOfEventLabel.autoPinEdge(.Left, toEdge: .Left, ofView: self.view, withOffset: 30)
+//        nameOfEventLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: titleLabel, withOffset: 10)
+        
+        userNameOfEvent.delegate = self
+        userNameOfEvent.backgroundColor = UIColor.whiteColor()
+        userNameOfEvent.text = "Event name"
+        userNameOfEvent.textColor = UIColor.lightGrayColor()
+        userNameOfEvent.autoSetDimension(.Height, toSize: 30)
+        userNameOfEvent.autoSetDimension(.Width, toSize: 200)
+        userNameOfEvent.layer.cornerRadius = 5
+        userNameOfEvent.autoPinEdge(.Top, toEdge: .Bottom, ofView: titleLabel, withOffset: 10)
+        userNameOfEvent.autoPinEdge(.Right, toEdge: .Right, ofView: self.view, withOffset: -30)
+        userNameOfEvent.autoPinEdge(.Left, toEdge: .Left, ofView: self.view, withOffset: 30)
+        
+        
 //        addEventContainer.addSubview(header)
 //        header.autoPinEdge(.Left, toEdge: .Left, ofView: self.view)
 //        header.autoPinEdge(.Right, toEdge: .Right, ofView: self.view)
@@ -70,6 +103,27 @@ class AddEventViewController: UIViewController{
 //    override func preferredStatusBarStyle() -> UIStatusBarStyle {
 //        return UIStatusBarStyle.LightContent
 //    }
+    func textViewDidBeginEditing(textView: UITextView){
+        if textView.textColor == UIColor.lightGrayColor() {
+            textView.text = nil
+            textView.textColor = UIColor.blackColor()
+        }
+    }
+    
+    func textViewDidEndEditing(textView: UITextView){
+        if textView.text.isEmpty {
+            textView.text = "Event Name"
+            textView.textColor = UIColor.lightGrayColor()
+        }
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
     
     func onExit(sender:UIButton!){
         self.dismissViewControllerAnimated(true, completion: nil)
